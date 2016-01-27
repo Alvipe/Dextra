35d62f09ae2096dbd10665f7a6968b0120cd322e
alvipe <alvaro.villoslada@bq.com>
2016-01-27 18:24:37 +0100

    Update finger_control.ino

--- Hand/Code/finger_control.ino
+++ Hand/Code/finger_control.ino
@@ -7,63 +7,70 @@ int dCycle = map(60, 0, 100, 0, 255);
 int abAngle = map(10, 0, 90, 20, 110);
 char control;
 int i;
-int flag;
+boolean clsd, opnd;
 
 void setup() {
     pinMode(phasePin, OUTPUT);
     pinMode(enablePin, OUTPUT);
     abductor.attach(3);
     abductor.write(abAngle);
+    clsd = false;
+    opnd = true;
     Serial.begin(115200);
 }
 
-void loop() {
-    while(Serial.available()) {
-        control = Serial.read();
-        flag = 0;
-    }
-    if(flag == 0) {
-        switch(control) {
-            case 'o':
-                openFinger();
-                break;
-            case 'c':
-                closeFinger();
-                break;
-            case '1':
-                abAngle = map(10, 0, 90, 20, 110);
-                abductor.write(abAngle);
-                break;
-            case '2':
-                abAngle = map(45, 0, 90, 20, 110);
-                abductor.write(abAngle);
-                break;
-            case '3':
-                abAngle = map(90, 0, 90, 20, 110);
-                abductor.write(abAngle);
-                break;
+void closeFinger() {
+    if(!clsd && opnd) {
+        digitalWrite(enablePin, HIGH);
+        for(i=0;i<10;i++) {
+            analogWrite(phasePin, dCycle);
+            delay(100);
         }
+        clsd = true;
+        opnd = false;
     }
     else {
         analogWrite(phasePin,0);
     }
 }
 
-void closeFinger() {
-    digitalWrite(enablePin, HIGH);
-    for(i=0;i<10;i++) {
-        analogWrite(phasePin, dCycle);
-        delay(100);
-    }
-    flag = 1;
-}
-
 void openFinger() {
-    digitalWrite(enablePin, LOW);
-    for(i=4;i>0;i--) {
-        analogWrite(phasePin, dCycle);
-        delay(100);
+    if(clsd && !opnd) {
+        digitalWrite(enablePin, LOW);
+        for(i=4;i>0;i--) {
+            analogWrite(phasePin, dCycle);
+            delay(100);
+        }
+        clsd = false;
+        opnd = true;
     }
-    flag = 1;
+    else {
+        analogWrite(phasePin,0);
+    }
 }
 
+void loop() {
+    while(Serial.available()) {
+        control = Serial.read();
+    }
+    switch(control) {
+        case 'o':
+            openFinger();
+            break;
+        case 'c':
+            closeFinger();
+            break;
+        case '1':
+            abAngle = map(10, 0, 90, 20, 110);
+            abductor.write(abAngle);
+            break;
+        case '2':
+            abAngle = map(45, 0, 90, 20, 110);
+            abductor.write(abAngle);
+            break;
+        case '3':
+            abAngle = map(90, 0, 90, 20, 110);
+            abductor.write(abAngle);
+            break;
+    }
+}
