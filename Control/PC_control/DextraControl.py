01e7eb1f0343980aabea654793405b86de2b3943
Alvipe <alvipe5@protonmail.com>
2016-08-23 19:41:01 +0200

    Update control interface

--- Control/PC_control/DextraControl.py
+++ Control/PC_control/DextraControl.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, ListProperty, StringProperty
+from kivy.properties import ObjectProperty, ListProperty
 from kivy.clock import Clock
 
 import sys
@@ -16,17 +15,14 @@ finger_address = [chr(0x01),chr(0x02),chr(0x03),chr(0x04),chr(0x05),chr(0x06)]
 class RootWidget(TabbedPanel):
     ser = None
     scheduled_event = None
-    setpoint_list = [10.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    slider_values = ListProperty(setpoint_list)
-    status = StringProperty("Not connected")
-    abductor = ObjectProperty(None)
-    thumb = ObjectProperty(None)
-    index = ObjectProperty(None)
-    middle = ObjectProperty(None)
-    ring = ObjectProperty(None)
-    pinky = ObjectProperty(None)
+    setpoint_list = ListProperty([10.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    serial_port_list = ListProperty([])
+    devices = ObjectProperty(None)
+    status = ObjectProperty(None)
 
-    def connect_to_serial(self):
+    def scan_ports(self):
+        self.devices.text = "Select device"
+        self.serial_port_list = []
         portname_start = ["/dev/ttyUSB","/dev/ttyACM","COM"]
         for port in portname_start:
             for i in range(10):
@@ -34,34 +30,29 @@ class RootWidget(TabbedPanel):
                 portname_full = ''.join([port,portnum])
                 try:
                     self.ser = serial.Serial(portname_full, 115200, timeout=1)
-                    self.ser.setDTR(False)
-                    time.sleep(1)
-                    self.ser.flushInput()
-                    self.ser.setDTR(True)
-                    time.sleep(2)
-                    self.status = "Connected"
-                    # return ser
-                    break
+                    self.ser.close()
+                    self.serial_port_list.append(portname_full)
                 except:
-                    if(i == 9 and port == "COM"):
-                        print("No serial port found")
-                        self.status = "No serial port found"
-                        # sys.exit(0)
+                    self.ser = None
 
-    def updateAbductor(self):
-        self.setpoint_list[0] = self.abductor.value
-    def updateThumb(self):
-        self.setpoint_list[1] = self.thumb.value
-    def updateIndex(self):
-        self.setpoint_list[2] = self.index.value
-    def updateMiddle(self):
-        self.setpoint_list[3] = self.middle.value
-    def updateRing(self):
-        self.setpoint_list[4] = self.ring.value
-    def updatePinky(self):
-        self.setpoint_list[5] = self.pinky.value
+    def connect_to_serial(self,serial_port):
+        try:
+            self.ser = serial.Serial(serial_port, 115200, timeout=1)
+            self.ser.setDTR(False)
+            time.sleep(1)
+            self.ser.flushInput()
+            self.ser.setDTR(True)
+            time.sleep(2)
+            self.status.text = "Connected"
+            self.status.color = [0,1,0,1]
+        except:
+            self.ser = None
+            self.status.text = "No serial port found"
+            self.status.color = [1,0,0,1]
+            print("No serial port found")
+
     def connect_press(self):
-        self.connect_to_serial()
+        self.connect_to_serial(self.devices.text)
         if(self.ser):
             self.scheduled_event = Clock.schedule_interval(self.send_setpoint_list, 0.1)
     def disconnect_press(self):
@@ -70,27 +61,27 @@ class RootWidget(TabbedPanel):
             Clock.unschedule(self.scheduled_event)
             # Clock.unschedule(self.send_setpoint_list) # METHOD NOT RECOMMENDED
             self.ser = None
-            self.status = "Disconnected"
+            self.status.text = "Disconnected"
+            self.status.color = [1,1,0,1]
         else:
-            self.status = "Not connected"
+            self.status.text = "Not connected"
+            self.status.color = [1,0,0,1]
     def send_setpoint_list(self,dt):
         message = header
         for i in range(len(self.setpoint_list)):
             data = struct.pack('f',float(self.setpoint_list[i]))
             message += finger_address[i] + data
         message += footer
-        if(self.ser.isOpen()):
+        try:
             self.ser.write(message)
             print self.setpoint_list
-        # else:
-        #     self.status = "Not connected"
+        except:
+            Clock.unschedule(self.scheduled_event)
+            print "Stopped"
 
 class DextraControlApp(App):
     def build(self):
-        # Clock.schedule_interval(RootWidget().send_setpoint_list, 0.1)
         return RootWidget()
 
 if __name__ == '__main__':
-    # ser = connect_to_serial()
     DextraControlApp().run()
-    # ser.close()
